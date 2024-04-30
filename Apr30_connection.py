56da13f38263281bd2ff2beab93777044ff5c816
raspberrypi4instant <mijk0421@gmail.com>
2024-04-30 14:27:44 +0900

    Modify Apr30.py

--- Apr30_connection.py
+++ Apr30_connection.py
@@ -3,27 +3,30 @@ import threading
 import time
 
 class SerialComm:
-    def init(self):
+    def __init__(self):
         self.comm=serial.Serial("/dev/ttyAMA0", 9600)
         self.isWorking=True
 
-        inimsg = ser.readline().decode().strip()
-        print(inimsg)
+        #self.inimsg = ser.readline().decode().strip()
+        #print(self.inimsg)
 
-    def write(self, alcoltrue):
-        if self.comm is None:
-            return 0
-        msg_alcol=str(alcoltrue)+'*'
-        self.comm.write(msg_alcol.encode())
+    def writer(self, alcoltrue):
+        if self.comm and self.comm.is_open:
+            msg_alcol=str(alcoltrue)+'*'
+            self.comm.write(msg_alcol.encode())
 
 ###############################################
 
 def main():
-    sc1=serial_comm()
+    sc1=SerialComm()
+    #sc1=serial_comm()
     def serial_read():
-        sc1.write(1)
-        if sc1.distance<10:
+        sc1.writer(1)
+        if sc1.distance < 10:
             pass
 
     serial_reader=threading.Thread(target = serial_read)
     serial_reader.start()
+    
+if __name__ == "__main__":
+    main()
