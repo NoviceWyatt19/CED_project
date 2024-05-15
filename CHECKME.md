5db2385a26bc70957e03ad08526093533520fea2
NoviceWyatt19 <aseltula596@gmail.com>
2024-05-15 23:53:31 +0900

    Update .gitignore to exclude .md files and refers folder ans Move sample files to sample folder

--- CHECKME.md
+++
@@ -1,1 +0,0 @@
-this md file is made at raspberrypi4 b
--- tset_webcam.py
+++ tset_webcam.py
@@ -42,7 +42,7 @@ def videoDetector(cam, cascade):
     cv2.destroyAllWindows()
 
 # 가중치 파일 경로
-cascade_filename = 'haarcascade_frontalface_alt.xml'
+cascade_filename = './sample/haarcascade_frontalface_alt.xml'
 # 모델 불러오기
 cascade = cv2.CascadeClassifier(cascade_filename)
 
