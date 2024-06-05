5e1f39caba0e83f031dada57b4ce075fed043b4d
NoviceWyatt19 <aseltula596@gmail.com>
2024-06-05 10:57:40 +0900

    Make main file in inTheMac branch

---
+++ in_the_Mac_main.py
@@ -0,0 +1,24 @@
+import threading
+import subprocess
+
+def run_eye_detect():
+    subprocess.run(["python", "in_the_Mav_eye.py"])
+
+def run_lane_detect():
+    subprocess.run(["python", "in_the_Mac_lane.py"])
+
+def main():
+    # 첫 번째 스레드에서 app_eye_detect.py 실행
+    eye_detect_thread = threading.Thread(target=run_eye_detect)
+    eye_detect_thread.start()
+
+    # 두 번째 스레드에서 app_lane_detect.py 실행
+    lane_detect_thread = threading.Thread(target=run_lane_detect)
+    lane_detect_thread.start()
+
+    # 모든 스레드가 종료될 때까지 대기
+    eye_detect_thread.join()
+    lane_detect_thread.join()
+
+if __name__ == "__main__":
+    main()
